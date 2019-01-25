c589a2a17d08d1edab1e7b56978fa14fb7998e97
ajle96 <alessandro.greco18@imperial.ac.uk>
2019-01-25 12:57:45 +0000

    renamed all star files to follow the underscore style

--- tests/test_STAR_readmap_nodocker.py
+++ tests/test_STAR_readmap_nodocker.py
@@ -5,8 +5,8 @@ def test_STAR_readmap_nodocker():
     """test_STAR_readmap_nodocker"""
     subprocess.run(["cwl-runner",
                     "--outdir=./test1",
-                    "./cwl-tools/nodocker/STAR-readmap.cwl",
-                    "./tests/STAR-readmap.yml"])
+                    "./cwl-tools/nodocker/STAR_readmap.cwl",
+                    "./tests/STAR_readmap.yml"])
     subprocess.run(["tail -n +5 ./test1/test1Aligned.out.sam > ./test1/test1.tail.sam"], shell=True)
     subprocess.run(["cat -A ./test1/test1.tail.sam"], shell=True)
     assert filecmp.cmp("./test1/test1.tail.sam", "./tests/test1.tail.sam")
