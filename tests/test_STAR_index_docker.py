0916157e74c9d3aabe1fac4a2d2ff18e97feba37
tonyyzy <38984697+tonyyzy@users.noreply.github.com>
2019-01-27 18:45:54 +0000

    Update test_STAR_index_docker.py

--- tests/test_STAR_index_docker.py
+++ tests/test_STAR_index_docker.py
@@ -4,11 +4,11 @@ import filecmp
 def test_STAR_index_docker():
     """test_STAR_index_nodocker"""
     subprocess.run(["cwl-runner",
-                    "--outdir=./STARIndex",
+                    "--outdir=./STARIndexd",
                     "./cwl-tools/docker/STAR_index.cwl",
                     "./tests/STAR_index.yml"])
 
-    assert filecmp.cmp("./tests/GenomeIndex/SA", "./STARIndex/SA")
+    assert filecmp.cmp("./tests/GenomeIndex/SA", "./STARIndexd/SA")
 
 
 if __name__ == "__main__":
