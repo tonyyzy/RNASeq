d3adcdbd9bad7186011c6c33e5003e419212c262
tonyyzy <38984697+tonyyzy@users.noreply.github.com>
2019-01-29 17:01:01 +0000

    Update test_STAR_readmap_nodocker.py

--- tests/test_STAR_readmap_nodocker.py
+++ tests/test_STAR_readmap_nodocker.py
@@ -4,7 +4,6 @@ import filecmp
 def test_STAR_readmap_nodocker():
     """test_STAR_readmap_nodocker"""
     subprocess.run(["cwl-runner",
-                    "--outdir=./test1",
                     "./cwl-tools/nodocker/STAR_readmap.cwl",
                     "./tests/STAR_readmap.yml"])
     subprocess.run(["tail -n +5 ./test1/test1Aligned.out.sam > ./test1/test1.tail.sam"], shell=True)
