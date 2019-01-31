949e7cd2ef0e042faa22d7b0e1499ceb158ad892
Mr Zeyu Yang <tony@tony.tc>
2019-01-31 15:20:18 +0000

    update tests

--- tests/test_STAR_readmap_docker.py
+++ tests/test_STAR_readmap_docker.py
@@ -4,11 +4,10 @@ import filecmp
 def test_STAR_readmap_docker():
     """test_STAR_readmap_nodocker"""
     subprocess.run(["cwl-runner",
-                    "--outdir=./test1d",
                     "./cwl-tools/docker/STAR_readmap.cwl",
-                    "./tests/STAR_readmap.yml"])
-    subprocess.run(["tail -n +5 ./test1d/test1Aligned.out.sam > ./test1d/test1.tail.sam"], shell=True)
-    assert filecmp.cmp("./test1d/test1.tail.sam", "./tests/test1.tail.sam")
+                    "./tests/STAR_readmap_docker.yml"])
+    subprocess.run(["tail -n +5 ./test3/test3Aligned.out.sam > ./test3/test3.tail.sam"], shell=True)
+    assert filecmp.cmp("./test3/test3.tail.sam", "./tests/test3.tail.sam")
 
 
 if __name__ == "__main__":
