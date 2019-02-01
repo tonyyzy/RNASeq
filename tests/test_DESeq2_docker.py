a7baa387af7db7af9d0e5d1bfb1f009a6d579450
ajle96 <alessandro.greco18@imperial.ac.uk>
2019-02-01 11:32:52 +0000

    Created test for DESeq2 docker version

---
+++ tests/test_DESeq2_docker.py
@@ -0,0 +1,14 @@
+import subprocess
+import filecmp
+
+def test_DESeq2_docker():
+
+    subprocess.run(["cwl-runner",
+                    "--outdir=./test_DESeq2_out",
+                    "./cwl-tools/docker/DESeq2.cwl",
+                    "./tests/DESeq2.yml"])
+
+    assert filecmp.cmp("./tests/DGE_results.csv", "./test_DESeq2_out/DGE_results.csv")
+
+if __name__ == "__main__":
+    test_DESeq2_docker()
