ef8daa1a209127a3f9fad99b97cda5b35827a916
tonyyzy <38984697+tonyyzy@users.noreply.github.com>
2019-02-11 17:56:05 +0000

    Add stringtie and DESeq2

--- backend_scripts/programs.py
+++ backend_scripts/programs.py
@@ -1,17 +1,45 @@
 import yaml
+from collections import OrderedDict
 
-cwl_workflow = {"cwl": {"cwlVersion": "v1.0",
-                    "class": "Workflow",
-                    "requirements":{
-                        "ScatterFeatureRequirement": {},
-                        "MultipleInputFeatureRequirement": {}},
-                    "inputs": {"Threads": "string"},
-                    "outputs": {},
-                    "steps": {}
+cwl_workflow = {"cwl":
+                    {
+                        "cwlVersion": "v1.0",
+                        "class": "Workflow",
+                        "requirements":{
+                            "ScatterFeatureRequirement": {},
+                            "MultipleInputFeatureRequirement": {},
+                            "StepInputExpressionRequirement": {},
+                            "InlineJavascriptRequirement": {}
+                            },
+                        "inputs": {
+                            "threads": "int"
+                            },
+                        "outputs": {},
+                        "steps": {}
                     },
-                "yml": {
-                    "threads": 2
-                }}
+                "yml":
+                    {
+                        "threads": 2
+                    }
+                }
+
+# cwl_workflow = {"cwl": {},
+#                 "yml": {}
+#                 }
+# 
+# cwl_workflow["cwl"]["cwlVersion"] = "v1.0"
+# cwl_workflow["cwl"]["class"] = "Workflow"
+# cwl_workflow["cwl"]["requirements"] = {"ScatterFeatureRequirement": {},
+#                                         "MultipleInputFeatureRequirement": {}
+#                                         }
+# cwl_workflow["cwl"]["inputs"] = {}
+# cwl_workflow["cwl"]["outputs"] = {}
+# cwl_workflow["cwl"]["steps"] = {}
+
+output_string = {
+    "samtools": "samtools_out",
+    "prepDE": "gene_count"
+}
 
 
 def star_readmap(input_files, yaml):
@@ -33,13 +61,81 @@ def samtools(input_files, yaml):
 def prepDE():
     pass
 
-def stringtie():
-    pass
+def stringtie(input_files, yaml, output_string, prev):
+    # inputs
+    None
+    # outputs
+    yaml["cwl"]["outputs"]["stringtie_out"] = {"type": "Directory",
+                                                "outputSource": "stringtie_folder/out"
+                                                }
+    # steps
+    for i in range(len(input_files)):
+        yaml["cwl"]["steps"][f"stringtie_{i + 1}"] = {
+            "run": "../../cwl-tools/docker/stringtie.cwl",
+            "in": {
+                "input_bam": f"{prev}_{i+1}/{output_string[prev]}",
+                "threads": "threads",
+                "annotation": "annotation",
+                "outfilename": {
+                    "source": [f"subject_name_{i+1}"],
+                    "valueFrom": "$(self + \".gtf\")"
+                }
+            },
+            "out": ["stringtie_out"]
+        }
+    expression_string = ",".join(f"input.file{j+1}" for j in range(len(input_files)))
+    yaml["cwl"]["steps"]["stringtie_folder"] = {
+        "run": {
+            "class": "ExpressionTool",
+            "requirements": {"InlineJavascriptRequirement": {}},
+            "inputs": {f"file{j+1}": "File" for j in range(len(input_files))},
+            "outputs": {"out": "Directory"},
+            "expression": f"|${{return{{\"out\":{{\"class\":\"Directory\",\"basename\":\"stringtie\",\"listing\":[{expression_string}]}};}}}}",
+          },
+        "in": {f"file{j+1}": f"stringtie_{j+1}/stringtie_out" for j in range(len(input_files))},
+        "out": ["out"]
+        }
+        
+        
+    return yaml
 
-def deseq2():
-    pass
+def deseq2(input_files, yaml, output_string, prev):
+    yaml["cwl"]["inputs"]["DESeq2_script"] = "File"
+    yaml["cwl"]["inputs"]["metadata"] = "File"
+    yaml["cwl"]["outputs"]["DESeq2_out"] = {
+        "type": "Directory",
+        "outputSource": "DESeq2_folder/out"
+    }
+    yaml["cwl"]["steps"]["DESeq2"] = {
+        "run": "../../cwl-tools/docker/DESeq2.cwl",
+        "in": {
+            "script": "DESeq2_script",
+            "count_matrix": f"{prev}/{output_string[prev]}",
+            "metadata": "metadata"
+        },
+        "out": ["DESeq2_out"]
+    }
+    yaml["cwl"]["steps"]["DESeq2_folder"] = {
+        "run": {
+            "class": "ExpressionTool",
+            "requirements": {"InlineJavascriptRequirement": {}},
+            "inputs": {"file1": "File"},
+            "outputs": {"out": "Directory"},
+            "expression": "|${return{\"out\":{\"class\":\"Directory\",\"basename\":\"DESeq2\",\"listing\":[inputs.file1]};}}",
+          },
+        "in": {"file1": "DESeq2/DESeq2_out"},
+        "out": ["out"]
+    }
+    return yaml
+
+
+inputs = {"test1": {"type": "paired_end", "path": {1: "test.fastq", 2: "test.fastq"}},
+          "test2": {"type": "paired_end", "path": {1: "test.fastq", 2: "test.fastq"}}}
+cwl_workflow = star_readmap(inputs, cwl_workflow)
+cwl_workflow = stringtie(inputs, cwl_workflow, output_string, "samtools")
+cwl_workflow = deseq2(inputs, cwl_workflow, output_string, "prepDE")
 
 with open("test.cwl", "w+") as outfile:
-    yaml.dump(star_readmap({"test1":{"type": "paired_end", "path":{1: "test.fastq", 2: "test.fastq"}},
-                            "test2":{"type": "paired_end", "path":{1: "test.fastq", 2: "test.fastq"}}},
-                             cwl_workflow) , outfile, default_flow_style=False)
+    yaml.dump(cwl_workflow["cwl"], outfile, default_flow_style=False)
+
+
