f6f5721deaa43aa2e779bcda6173d07b78f1adbe
ajle96 <alessandro.greco18@imperial.ac.uk>
2019-02-11 17:30:08 +0000

    added star folder and samtools + samtools folder

--- backend_scripts/programs.py
+++ backend_scripts/programs.py
@@ -4,8 +4,10 @@ cwl_workflow = {"cwl": {"cwlVersion": "v1.0",
                     "class": "Workflow",
                     "requirements":{
                         "ScatterFeatureRequirement": {},
-                        "MultipleInputFeatureRequirement": {}},
-                    "inputs": {"Threads": "string"},
+                        "MultipleInputFeatureRequirement": {},
+                        "StepInputExpressionRequirement": {},
+                        "InlineJavascriptRequirement": {}},
+                    "inputs": {"threads": "string"},
                     "outputs": {},
                     "steps": {}
                     },
@@ -17,18 +19,65 @@ cwl_workflow = {"cwl": {"cwlVersion": "v1.0",
 def star_readmap(input_files, yaml):
     for i in range(len(input_files)):
         yaml["cwl"]["inputs"]["genomeDir"] = "Directory"
-        yaml["cwl"]["inputs"]["readFilesIn_{0}".format(i)] = "File[]"
-        yaml["cwl"]["inputs"]["outFileNamePrefix_{0}".format(i)] = "string"
-        yaml["cwl"]["outputs"]["star_readmap_1_out"] = {"type": "Directory", "outputSource": "star_readmap_{0}/star_read_out".format(i)}
-        yaml["cwl"]["steps"]["star_readmap_{0}".format(i)] = {"run": "../cwl-tools/docker/STAR_readmap.cwl",
-        "in": {"Threads": "Threads",
-      "genomeDir": "genomeDir",
-      "readFilesIn": "readFilesIn_{0}".format(i),
-      "outFileNamePrefix": "outFileNamePrefix_{0}".format(i)}, "out": ["sam_output", "star_read_out"]}
+        yaml["cwl"]["inputs"]["subject_name{0}".format(i + 1)] = "File[]"
+        yaml["cwl"]["inputs"]["fastq{0}".format(i + 1)] = "string"
+        yaml["cwl"]["outputs"]["star_readmap_1_out"] = {
+        "type": "Directory",
+        "outputSource": "star_readmap_{0}/star_read_out".format(i + 1)}
+        yaml["cwl"]["steps"]["star_readmap_{0}".format(i + 1)] = {
+        "run": "../cwl-tools/docker/STAR_readmap.cwl",
+        "in": {
+            "threads": "threads",
+            "genomeDir": "genomeDir",
+            "readFilesIn": "fastq{0}".format(i + 1),
+            "outFileNamePrefix": "subject_name{0}".format(i + 1)},
+        "out": ["sam_output", "star_read_out"]}
+        yaml["yml"]["genomeDir"] = "Directory"
+        yaml["yml"]["readFilesIn_{0}".format(i + 1)] = "File[]"
+        yaml["yml"]["outFileNamePrefix_{0}".format(i + 1)] = "string"
+
+    yaml["cwl"]["steps"]["star_folder"] = {
+    "run": {
+        "class": "ExpressionTool",
+        "requirements": {
+            "InlineJavascriptRequirement": {}},
+        "inputs": dict([("dir{0}".format(i + 1), "Directory") for i in range(len(input_files))]),
+        "outputs": {
+            "out": "Directory"},
+        "expression": "|${{return{{\"out\":{{\"class\":\"Directory\",\"basename\":\"star\",\"listing\":[{}]}};}}}}".format(",".join(["inputs.dir{0}".format(i + 1) for i in range(len(input_files))]))},
+    "in": dict([("dir{0}".format(i + 1), "star_readmap_{0}/star_read_out".format(i + 1)) for i in range(len(input_files))]),
+    "out": ["out"]
+    }
+
     return yaml
 
 def samtools(input_files, yaml):
-    pass
+    for i in range(len(input_files)):
+        yaml["cwl"]["steps"]["samtools_{0}".format(i + 1)] = {
+        "run": "../cwl-tools/docker/samtools.cwl",
+        "in": {
+            "samfile": "star_readmap_{0}/sam_output".format(i + 1),
+            "threads": "threads",
+            "outfilename": {
+                "source": ["subject_name{0}".format(i + 1)],
+                "valueFrom": "$(self + \".bam\")"}},
+        "out": ["samtools_out"]}
+
+    yaml["cwl"]["steps"]["samtools_folder"] = {
+    "run": {
+        "class": "ExpressionTool",
+        "requirements": {
+            "InlineJavascriptRequirement": {}},
+        "inputs": dict([("file{0}".format(i + 1), "File") for i in range(len(input_files))]),
+        "outputs": {
+            "out": "Directory"},
+        "expression": "|${{return{{\"out\":{{\"class\":\"Directory\",\"basename\":\"samtools\",\"listing\":[{}]}};}}}}".format(",".join(["inputs.file{0}".format(i + 1) for i in range(len(input_files))]))},
+    "in": dict([("file{0}".format(i + 1), "samtools_{0}/samtools_out".format(i + 1)) for i in range(len(input_files))]),
+    "out": ["out"]
+    }
+
+    return yaml
+
 
 def prepDE():
     pass
@@ -39,7 +88,13 @@ def stringtie():
 def deseq2():
     pass
 
+
+
+input_file = {"test1":{"type": "paired_end", "path":{1: "test.fastq", 2: "test.fastq"}},
+              "test2":{"type": "paired_end", "path":{1: "test.fastq", 2: "test.fastq"}}}
+ale = star_readmap(input_file, cwl_workflow)
+ale = samtools(input_file, ale)
 with open("test.cwl", "w+") as outfile:
-    yaml.dump(star_readmap({"test1":{"type": "paired_end", "path":{1: "test.fastq", 2: "test.fastq"}},
-                            "test2":{"type": "paired_end", "path":{1: "test.fastq", 2: "test.fastq"}}},
-                             cwl_workflow) , outfile, default_flow_style=False)
+    yaml.dump(ale['cwl'], outfile, default_flow_style=False)
+with open("test.yml", "w+") as outfile:
+    yaml.dump(ale['yml'], outfile, default_flow_style=False)
