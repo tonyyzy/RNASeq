2e9e29de27d2bb200efbf02895cf18a5ac8dabd5
patrick hedley-miller <patrickhedleymiller@gmail.com>
2019-02-21 16:02:55 +0000

    javascript form user interaction (saving from isar 19 graphics failuregit add -A

--- webportal/static/js/myscript.js
+++ webportal/static/js/myscript.js
@@ -1,8 +1,81 @@
-$('h1').click(function(){
-console.log('there was a click')
-})
+// $('h1').click(function(){
+// console.log('there was a click')
+// })
 
 
 $('.dropdown-item').click(function(){
 console.log('there was a click')
 })
+
+
+
+$('#div_id_libtype option:eq(2)').click(function(){
+  $(this).text()=='Blue';
+})
+
+
+// $('[name=options] option').filter(function() {
+//     return ($(this).text() == 'Blue'); //To select Blue
+// }).prop('selected', true);
+//
+//
+// $('#div_id_libtype option:eq(2)')
+
+// $('#div_id_libtype option:eq(2)').css("background-color", "yellow");
+
+
+
+// SAMPLES CREATE JAVASCRIPT
+$( document ).ready(function() {
+  var user_url = window.location.pathname
+  if (user_url.includes('samples_create')){ // executes if samples_create page loaded
+    $('#div_id_read_1').hide()
+    $('#div_id_read_2').hide()
+    $('#div_id_accession').hide()
+    $('#div_id_libtype').on('click', function(){
+      if ($('#div_id_libtype option:selected').val() == 'PE'){ // executes if PE selected from dropdown
+        $('#div_id_read_1').hide()
+        $('#div_id_read_2').hide()
+        $('#div_id_accession').hide()
+        $('#div_id_read_1').show()
+        $('#div_id_read_2').show()
+        $('#div_id_accession').show()
+      }
+      if ($('#div_id_libtype option:selected').val() == 'SG'){ // executes if SG selected from dropdown
+        $('#div_id_read_1').hide()
+        $('#div_id_read_2').hide()
+        $('#div_id_accession').hide()
+        $('#div_id_read_1').show()
+        $('#div_id_accession').show()
+      }
+    })
+  }
+  else {
+    console.log('no blinding required')
+  }
+});
+
+// SAMPLES UPDATE JAVASCRIPT
+$( document ).ready(function() {
+  var user_url = window.location.pathname // stores current url as user_url
+  if (user_url.includes('samples_update')){ // executes if samples_update page loaded
+    if ($('#div_id_libtype option:selected').val() == 'SG'){
+    	$('#div_id_read_2').hide()
+    }
+    $('#div_id_libtype').on('click', function(){
+      if ($('#div_id_libtype option:selected').val() == 'PE'){ // executes if PE selected from dropdown
+        $('#div_id_read_2').show()
+        $('#read_2-clear_id').prop("checked", false);
+      }
+      if ($('#div_id_libtype option:selected').val() == 'SG'){ // executes if PE selected from dropdown
+        $('#div_id_read_2').hide()
+        $('#read_2-clear_id').prop("checked", true);
+      }
+    })
+    }
+  else {
+    console.log('no hiding required')
+  }
+});
+
+id="read_2-clear_id"
