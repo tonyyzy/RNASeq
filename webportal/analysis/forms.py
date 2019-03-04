69a5200a9f991a2631993be3b97b0f18227a7936
patrick hedley-miller <patrickhedleymiller@gmail.com>
2019-03-04 15:49:01 +0000

    saving from GPU crash 2.0

--- webportal/analysis/forms.py
+++ webportal/analysis/forms.py
@@ -17,13 +17,13 @@ class SessionForm(forms.ModelForm):
 
     class Meta:
         model = Session
-        fields = ['genome_index','select_genome','organism', 'salmon', 'fasta_dna_file', 'fasta_cdna_file', 'gtf_file', 'status']
+        fields = ['genome_index','genome','organism', 'salmon', 'fasta_dna_file', 'fasta_cdna_file', 'gtf_file', 'status']
         # fields='__all__'
         widgets={
             'genome_index': forms.Select(attrs={
                 'class':'form-control',
                 }),
-            'select_genome': forms.Select(attrs={
+            'genome': forms.Select(attrs={
                 'class':'form-control',
                 }),
             'organism': forms.TextInput(attrs={
--- webportal/webportal/settings.py
+++ webportal/webportal/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'tem51!gzk)#%&4#v0)bh*&ano&*0k7n1nr6at3elfdbu4q4ad-'
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['127.0.0.1', '155.198.233.29', '155.198.232.174']
+ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1', '155.198.233.29', '155.198.232.174']
 
 '''
 IP ADDRESS INFO
@@ -99,14 +99,14 @@ WSGI_APPLICATION = 'webportal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'rnaseq',
-        # 'USER': 'rnasequser',
-        # 'PASSWORD': 'RnaSeq123!',
-        # 'HOST': 'bmmbackup',
-        # 'PORT': '3306',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rnaseq',
+        'USER': 'rnasequser',
+        'PASSWORD': 'RnaSeq123!',
+        'HOST': 'bmmbackup',
+        'PORT': '3306',
     }
 }
 
