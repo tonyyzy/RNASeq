ec37377fd03d958303ed34ca9c7f27b955de265b
patrick hedley-miller <patrickhedleymiller@gmail.com>
2019-03-14 16:43:15 +0000

    updated visualization page

--- webportal/static/js/my_volcano.js
+++ webportal/static/js/my_volcano.js
@@ -1,4 +1,4 @@
-
+var all_circles;
 
 $( document ).ready(function() {
    p_val = 0.05
@@ -12,23 +12,42 @@ $( document ).ready(function() {
 });
 
 $("#id_p_threshold").change(function() {
+  d3.select("#id_painting_volcano").selectAll("line").remove()
   p_val = $("#id_p_threshold").val()/100
   p_threshold = - Math.log10(p_val);
   var output = document.getElementById("id_p_threshold_out");
   output.innerHTML = p_val;
   // console.log(p_threshold)
+  all_circles.attr("fill", function(d){
+          if(- Math.log10(d.padj) > p_threshold && (d.log2FoldChange > log2_threshold || d.log2FoldChange <  - log2_threshold)){
+            return colours[d.dataset_index];
+          } else {
+            return "grey";
+          };
+        })
   hLines()
+  vLines()
   return p_threshold
 });
 
 
 $("#id_log2_threshold").change(function() {
+  d3.select("#id_painting_volcano").selectAll("line").remove()
   log2_threshold = $("#id_log2_threshold").val()
   console.log(log2_threshold)
+  var output = document.getElementById("id_log2_threshold_out");
+  output.innerHTML = log2_threshold;
+  all_circles.attr("fill", function(d){
+          if(- Math.log10(d.padj) > p_threshold && (d.log2FoldChange > log2_threshold || d.log2FoldChange <  - log2_threshold)){
+            return colours[d.dataset_index];
+          } else {
+            return "grey";
+          };
+        })
   vLines()
+  hLines()
   return log2_threshold
 });
-
 
 
 colours = [
@@ -49,8 +68,8 @@ colours = [
   "#FFFF6D"];
 
 
-dataset = []
 function wf_select(param){
+  dataset = []
   result = []
   var all_checkboxes = $('.checkbox_wf')
   for(var i = 0; i < all_checkboxes.length; i++){
@@ -69,18 +88,31 @@ function wf_select(param){
   d3_run(endpoint)
 }
 
+// var volcanoplot = function() {
+//   var dataset = [];
+//     for (var i = 0; i < all_data.length; i++) {
+//       dataset.push(d3.csv("DGE_results_" + all_data[i] + ".csv", function(dataset){
+//         dataset.gene_name = dataset[""];
+//         dataset.Base_Mean = parseFloat(dataset.baseMean);
+//         dataset.log2FoldChange = parseFloat(dataset.log2FoldChange);
+//         dataset.pvalue = parseFloat(dataset.pvalue);
+//         dataset.padj = parseFloat(dataset.padj);
+//         return dataset;
+//       }));
+// };
+
 
 function d3_run(endpoint){
   console.log(endpoint)
   d3.json(endpoint).then(function(data) {
     console.log('d3_run called')
     dataset.push(data)
-    dataFilter()
+    dataFilter_volcano()
   });
 }
 
 
-function dataFilter(){
+function dataFilter_volcano(){
   dataset = dataset[0]
   dataset = dataset.filter(function(d){return ! isNaN(d.padj)});
   // myPlot()
@@ -88,23 +120,30 @@ function dataFilter(){
 }
 
 function newPlot(){
+  console.log(dataset);
+
   var margin = {top: 40, right: 20, bottom: 20, left: 40};
       //Width and height
-      w = 500 - margin.right - margin.left;
-      h = 500 - margin.top - margin.bottom;
+      w = 800 - margin.right - margin.left;
+      h = 800 - margin.top - margin.bottom;
 
       xScale = d3.scaleLinear()
-      // .domain([d3.min(dataset, function(d) { return d.log2FoldChange - 0.5;}),
-               // d3.max(dataset, function(d) { return d.log2FoldChange + 0.5;})])
-
-      .domain([d3.min(dataset, function(d) { return - d.log2FoldChange - 7;}),
-               d3.max(dataset, function(d) { return - d.log2FoldChange + 7;})])
+        .domain([d3.min(dataset, function(d) { return  d.log2FoldChange - 2;}),
+                 d3.max(dataset, function(d) { return  d.log2FoldChange + 2;})])
       .range([0, w]);
+
+      d3.select("id_p_threshold")
+        .attr("min", "" + d3.min(dataset, function(d) { return d.log2FoldChange;}))
+        .attr("max", "" + d3.max(dataset, function(d) { return d.log2FoldChange;}));
 
       yScale = d3.scaleLinear()
       .domain([d3.min(dataset, function(d) { return - Math.log10(d.padj) - 0.5;}),
                d3.max(dataset, function(d) { return - Math.log10(d.padj) + 0.5;})])
       .range([h, 0]);
+
+      d3.select("id_p_threshold")
+      .attr("min", "" + d3.min(dataset, function(d) { return - Math.log10(d.padj);}))
+      .attr("max", "" + d3.max(dataset, function(d) { return - Math.log10(d.padj);}));
 
       xAxis = d3.axisBottom()
       .scale(xScale);
@@ -112,8 +151,8 @@ function newPlot(){
 
       yAxis = d3.axisLeft()
       .scale(yScale);
-
-      svg = d3.select("#painting")
+      // d3.select("#id_painting_volcano").selectAll("*").remove();
+      svg = d3.select("#id_painting_volcano")
       .append("svg")
       .attr("width", w + margin.right + margin.left) // <-- Here
       .attr("height", h + margin.top + margin.bottom)
@@ -129,44 +168,59 @@ function newPlot(){
       .attr("class", "axis") //Assign "axis" class
       .call(yAxis)
       circles()
+      hLines()
+      vLines()
 }
 
 function circles(){
+  console.log(dataset)
   svg.selectAll("circle").remove()
-  var circles = svg.selectAll('circle')
-      .data(dataset)
-      .enter().append('circle')
-    .attr('cx',function (d) { return xScale(d.log2FoldChange) })
-    .attr('cy',function (d) { return yScale(- Math.log10(d.padj)) })
-    .attr('r','5')
-    // .attr('cy',function (d) { return yScale(d.aror) })
-    // .attr('stroke','black')
-    // .attr('stroke-width',1)
-    // .attr('fill',function (d,i) { return colorScale(i) })
-
-      circles.attr("cx", function(d) {
-      return xScale(d.log2FoldChange);
-      })
-      .attr("cy",function(d){
-        return yScale(- Math.log10(d.padj));
-      })
-      .attr("r", 5)
-      .attr("fill", function(d){
-        if(- Math.log10(d.padj) > p_threshold && (d.log2FoldChange > log2_threshold || d.log2FoldChange <  - log2_threshold)){
-          return colours[d.dataset];
-        } else {
-          return "grey";
-        };
-      });
-      // vLines()
-      // hLines()
+        all_circles = svg.selectAll("circle")
+        .data(dataset)
+        .enter()
+        .append("circle")
+        .attr("cx", function(d) {
+          return xScale(d.log2FoldChange);
+        })
+        .attr("cy",function(d){
+          return yScale(- Math.log10(d.padj));
+        })
+        .attr("r", 5)
+        .attr("fill", function(d){
+          if(- Math.log10(d.padj) > p_threshold && (d.log2FoldChange > log2_threshold || d.log2FoldChange <  - log2_threshold)){
+            return colours[d.dataset_index];
+          } else {
+            return "grey";
+          };
+        })
+        .on("click", function() {
+        //Do something mundane and annoying on click
+        alert("Hey, don't click that!");
+        })
+        .on("mouseover", function(d) {
+          d3.select("#label").remove();
+          d3.select(this)
+          .attr("fill", "orange");
+          svg.append("text")
+            .attr("id", "label")
+            .attr("y", h + 35 )
+            .text("Gene: " + d[""] + " L2fc: " + d.log2FoldChange + " p adj: " + d.padj);
+        })
+        .on("mouseout", function(d){
+          d3.select(this).attr("fill", function(d){
+            if(- Math.log10(d.padj) > p_threshold && (d.log2FoldChange > log2_threshold || d.log2FoldChange <  - log2_threshold)){
+              return colours[d.dataset_index];
+            } else {
+              return "grey";
+            };
+          });
+        });
 }
 
 
 
 
 function vLines(){
-  svg.selectAll("line").remove()
   var v_lines_g = svg.append("g")
   .attr("class", "lines");
 
@@ -183,25 +237,23 @@ function vLines(){
   .attr("y2", "" + h)
   .attr("stroke", "red")
   .attr("stroke-width", "2");
-  circles()
 }
-//
-// function hLines(){
-//   svg.selectAll("line").remove()
-//   var h_lines_g = svg.append("g")
-//   .attr("class", "lines");
-//
-//   h_line = [p_threshold]
-//   var h_lines = h_lines_g.selectAll("line")
-//   .data(h_line)
-//   .enter()
-//   .append("line")
-//   .attr("x1", "0")
-//   .attr("y1", function(d){
-//     return yScale(d);})
-//   .attr("x2", "" + w)
-//   .attr("y2",function(d){ return yScale(d);})
-//   .attr("stroke", "red")
-//   .attr("stroke-width", "2");
-//   circles()
-// }
+
+function hLines(){
+  var h_lines_g = svg.append("g")
+  .attr("class", "lines");
+
+  h_line = [p_threshold]
+  var h_lines = h_lines_g.selectAll("line")
+  .data(h_line)
+  .enter()
+  .append("line")
+
+  .attr("x1", "0")
+  .attr("y1", function(d){
+    return yScale(d);})
+  .attr("x2", "" + w)
+  .attr("y2",function(d){ return yScale(d);})
+  .attr("stroke", "red")
+  .attr("stroke-width", "2");
+}
